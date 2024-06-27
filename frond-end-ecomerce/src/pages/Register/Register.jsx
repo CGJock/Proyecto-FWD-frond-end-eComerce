8b0852ebcea061120de088746b57f71dbb1f30b3
CGJock <cjocksanest@gmail.com>
2024-06-27 15:13:58 -0600

    validaciones de usuarios dupicados en el registro

--- frond-end-ecomerce/src/pages/Register/Register.jsx
+++ frond-end-ecomerce/src/pages/Register/Register.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Registform from "../../components/RegForm"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+    <Registform/>
+    </>
   )
 }
 
